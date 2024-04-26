988fe8734e534fce60a653f53e3afb5fb4684457
Victor Ozoterio <victorozoterio@gmail.com>
2024-04-26 16:05:40 -0300

    Senha criptografada

--- src/app/users/users.entity.ts
+++ src/app/users/users.entity.ts
@@ -1,27 +1,43 @@
-import { Column, CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-
-export class UsersEntity {
+import {
+    BeforeInsert,
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+  } from 'typeorm';
+  import { hashSync } from 'bcrypt';
+  
+  @Entity({ name: 'users' })
+  export class UsersEntity {
     @PrimaryGeneratedColumn('uuid')
     id: string;
-
+  
     @Column({ name: 'first_name' })
     firstName: string;
-
+  
     @Column({ name: 'last_name' })
     lastName: string;
-
+  
     @Column()
     email: string;
-
+  
     @Column()
-    password:string;
-
+    password: string;
+  
     @CreateDateColumn({ name: 'created_at' })
-    createdAt:string;
-
+    createdAt: string;
+  
     @UpdateDateColumn({ name: 'updated_at' })
-    updatedAt:string;
-
+    updatedAt: string;
+  
     @DeleteDateColumn({ name: 'deleted_at' })
-    deletedAt:string;
-}
+    deletedAt: string;
+  
+    @BeforeInsert()
+    hashPassword() {
+      this.password = hashSync(this.password, 10);
+    }
+  }
+  
